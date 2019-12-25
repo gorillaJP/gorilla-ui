71f9868dfca45e43a779c6ed59981791ef46a865
shashithd <shashithd@gmail.com>
2019-12-26 00:40:17 +0800

    Mobile menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -7,16 +7,31 @@ import LandingPage from "./components/pages/landing-page/LandingPage";
 import Footer from "./components/common/footer/Footer";
 
 function App() {
+    const [mobileMenuOpen, toggleMobileMenu] = useState(false);
+    const [userLoggeIn, setUserLoggedIn] = useState(false);
+
     return (
         <div className="App">
             <Router>
-                <Header />
-                <div className="content-wrapper">
-                    <Switch>
-                        <Route exact path="/" render={() => <LandingPage />} />
-                    </Switch>
-                </div>
-                <Footer />
+                <Header
+                    mobileMenuOpen={mobileMenuOpen}
+                    toggleMenu={val => {
+                        toggleMobileMenu(val);
+                    }}
+                    userLoggeIn={userLoggeIn}
+                    setUserLoggedIn={setUserLoggedIn}
+                />
+                {!mobileMenuOpen && (
+                    <>
+                        <div className="content-wrapper">
+                            <Switch>
+                                <Route exact path="/" render={() => <LandingPage />} />
+                            </Switch>
+                        </div>
+
+                        <Footer />
+                    </>
+                )}
             </Router>
         </div>
     );
--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Button, Row, Col, Icon } from "antd";
 import { Link } from "react-router-dom";
-
 import { Menu, Dropdown } from "antd";
+
+import MobileMenu from "./MobileMenu";
 import styles from "./Header.module.css";
 
 const LinkDropDownContent = items => {
@@ -21,121 +22,130 @@ const LinkDropDownContent = items => {
     );
 };
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userLoggeIn: false,
-            openSideMenu: false,
-            items: [
-                {
-                    linkName: "Jobs By Skill",
-                    linkPath: "/jobs-by-skill"
-                },
-                {
-                    linkName: "Jobs By Company",
-                    linkPath: "/jobs-by-company"
-                },
-                {
-                    linkName: "Jobs By Function",
-                    linkPath: "/jobs-by-function"
-                }
-            ]
-        };
+const jobsByFunctionItems = [
+    {
+        linkName: "Jobs By Skill",
+        linkPath: "/jobs-by-skill"
+    },
+    {
+        linkName: "Jobs By Company",
+        linkPath: "/jobs-by-company"
+    },
+    {
+        linkName: "Jobs By Function",
+        linkPath: "/jobs-by-function"
     }
+];
 
-    render() {
-        return (
-            <header>
-                <nav className={styles.header}>
-                    <Row gutter={[8, 8]}>
-                        <Col
-                            xs={4}
-                            sm={4}
-                            md={1}
-                            lg={1}
-                            xl={1}
-                            push={1}
-                            className={styles.mobileMenu}
-                        >
+const Header = props => {
+    return (
+        <header>
+            <nav className={styles.header}>
+                <Row gutter={[8, 8]}>
+                    <Col
+                        xs={4}
+                        sm={4}
+                        md={1}
+                        lg={1}
+                        xl={1}
+                        push={1}
+                        className={styles.mobileMenu}
+                    >
+                        {!props.mobileMenuOpen ? (
                             <Icon
                                 type="menu"
                                 onClick={() => {
-                                    this.setState({ openSideMenu: !this.state.openSideMenu });
+                                    props.toggleMenu(true);
                                 }}
+                                className={styles.dropDownButton}
                             />
-                        </Col>
-                        <Col
-                            xs={14}
-                            sm={14}
-                            md={4}
-                            lg={3}
-                            className={styles.headerIcon}
-                            push={1}
-                        >
-                            <a href="/">Gorilla</a>
-                        </Col>
-                        <Col
-                            sm={24}
-                            xs={24}
-                            md={11}
-                            lg={13}
-                            className={styles.desktopMenu}
-                            push={1}
-                        >
-                            <Row justify="start" type="flex">
-                                <div>
-                                    <Dropdown overlay={LinkDropDownContent(this.state.items)}>
-                                        <span className={styles.submenu}>Job Search</span>
-                                    </Dropdown>
-                                </div>
-                                <div>
-                                    <span className={styles.submenu}>Job Post</span>
-                                </div>
-                            </Row>
-                        </Col>
-                        <Col
-                            sm={24}
-                            xs={24}
-                            md={8}
-                            lg={6}
-                            className={styles.desktopMenu}
-                            push={1}
-                        >
-                            {!this.state.userLoggeIn && (
-                                <Row justify="end" type="flex">
-                                    <div className={styles.loginButton}>
-                                        <Button type="primary" icon="login" size="large">
-                                            <span>Login</span>
-                                        </Button>
-                                    </div>
-                                    <div className={styles.signupButton}>
-                                        <Button type="primary" icon="user-add" size="large">
-                                            <span>Signup</span>
-                                        </Button>
-                                    </div>
-                                </Row>
-                            )}
-
-                            {this.state.userLoggeIn && (
-                                <div className={styles.logoutButton}>
-                                    <Button type="primary" icon="logout" size="large">
-                                        <span>Logout</span>
+                        ) : (
+                            <Icon
+                                type="close"
+                                onClick={() => {
+                                    props.toggleMenu(false);
+                                }}
+                                className={styles.dropDownButton}
+                            />
+                        )}
+                    </Col>
+                    <Col
+                        xs={14}
+                        sm={14}
+                        md={4}
+                        lg={3}
+                        className={styles.headerIcon}
+                        push={1}
+                    >
+                        <a href="/">Gorilla</a>
+                    </Col>
+                    <Col
+                        sm={24}
+                        xs={24}
+                        md={11}
+                        lg={13}
+                        className={styles.desktopMenu}
+                        push={1}
+                    >
+                        <Row justify="start" type="flex">
+                            <div>
+                                <Dropdown overlay={LinkDropDownContent(jobsByFunctionItems)}>
+                                    <span className={styles.submenu}>Job Search</span>
+                                </Dropdown>
+                            </div>
+                            <div>
+                                <span className={styles.submenu}>Job Post</span>
+                            </div>
+                        </Row>
+                    </Col>
+                    <Col
+                        sm={24}
+                        xs={24}
+                        md={8}
+                        lg={6}
+                        className={styles.desktopMenu}
+                        push={1}
+                    >
+                        {!props.userLoggeIn && (
+                            <Row justify="end" type="flex">
+                                <div className={styles.loginButton}>
+                                    <Button type="primary" icon="login" size="large">
+                                        <span>Login</span>
                                     </Button>
                                 </div>
-                            )}
-                        </Col>
-                        <Col xs={6} sm={6} className={styles.mobileMenu}>
-                            <Row justify="end" type="flex">
-                                <Icon type="login" />
-                                <Icon type="user-add" />
+                                <div className={styles.signupButton}>
+                                    <Button type="primary" icon="user-add" size="large">
+                                        <span>Signup</span>
+                                    </Button>
+                                </div>
                             </Row>
-                        </Col>
+                        )}
+
+                        {props.userLoggeIn && (
+                            <div className={styles.logoutButton}>
+                                <Button type="primary" icon="logout" size="large">
+                                    <span>Logout</span>
+                                </Button>
+                            </div>
+                        )}
+                    </Col>
+                    <Col xs={6} sm={6} className={styles.mobileMenu}>
+                        <Row justify="end" type="flex">
+                            <Icon type="login" className={styles.loginIcon} />
+                            <Icon type="user-add" className={styles.registerIcon} />
+                        </Row>
+                    </Col>
+                </Row>
+                {props.mobileMenuOpen && (
+                    <Row>
+                        <div className={styles.leftMenu}>
+                            <MobileMenu />
+                        </div>
                     </Row>
-                </nav>
-            </header>
-        );
-    }
-}
+                )}
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
