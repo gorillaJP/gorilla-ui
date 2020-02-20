e6a702c80a1633570c90df8de8a4072fe0454362
dim912 <dim912@gmail.com>
2020-02-21 00:18:44 +0800

    pointed to http at server side

--- src/api/AutoCompleteApi.js
+++ src/api/AutoCompleteApi.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const cityAutoComplete = async value => {
     return await axios.get(
-        "https://159.89.161.233:443/api/meta/allcities?q=" + value
+        "http://159.89.161.233:443/api/meta/allcities?q=" + value
     );
 };
 
 export const sectorAutoComplete = async value => {
     return await axios.get(
-        "https://159.89.161.233/api/meta/allsectors?q=" + value
+        "http://159.89.161.233/api/meta/allsectors?q=" + value
     );
 };
