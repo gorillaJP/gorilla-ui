4894d3989bebbdfd0b4281ea79bfda3b1f6361d2
dim912 <dim912@gmail.com>
2020-03-06 01:20:21 +0800

    remove console.log

--- src/reducers/CommonReducer.js
+++ src/reducers/CommonReducer.js
@@ -4,7 +4,6 @@ import * as actions from '../actions/ActionTypes';
 export default function commonReducer(state = initialState.commonData, action) {
     switch (action.type) {
         case actions.LOADING:
-            console.log(action);
             return { ...state, ...{ loading: action.data } };
         default:
             return state;
