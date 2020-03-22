5cfb937c4537db11e4e324c43d2dc899c98c1855
shashithd <shashithd@gmail.com>
2020-03-22 12:31:47 +0800

    Text highlight feature

--- src/components/common/highlighted-text/HighLightedText.jsx
+++ src/components/common/highlighted-text/HighLightedText.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import { createMarkUp } from "../../../util";
 
 const HighLightedText = props => {
-    if (props.highlightText) {
-        const regex = new RegExp(props.highlightText, "ig");
-        const newTextStr = props.text.replace(regex, `<b>${props.highlightText}</b>`);
+    const regex = new RegExp(`^${props.highlightText}`, "i");
+    if (props.highlightText && regex.test(props.text)) {
+        const newTextStr = `${props.highlightText}<b>${props.text.replace(regex, "")}</b>`;
         return <span dangerouslySetInnerHTML={createMarkUp(newTextStr)}></span>;
     } else {
         return <span>{props.text}</span>;
