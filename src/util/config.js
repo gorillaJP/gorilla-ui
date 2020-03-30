4d390b35f706ab407ea71ffa0b3eefbb7c876002
dim912 <dim912@gmail.com>
2020-03-31 03:13:43 +0800

    uat set up

--- src/util/config.js
+++ src/util/config.js
@@ -5,20 +5,23 @@ let devConfig = (config = {
 });
 
 let uatConfig = (config = {
-    remote: 'http://localhost:8080/'
+    remote: 'http://159.89.161.233:443/'
 });
 
 let prodConfig = (config = {
     remote: 'http://159.89.161.233:443/'
 });
 
-//conf as per the env
-if (process.env.NODE_ENV === 'development') {
-    config = devConfig;
-} else if (process.env.NODE_ENV === 'uat') {
+if (process.env.REACT_APP_ENV === 'uat') {
+    //since it is not allowed to set node env directly, this alternative is vaiable is used
     config = uatConfig;
-} else if (process.env.NODE_ENV === 'production') {
-    config = prodConfig;
+} else {
+    if (process.env.NODE_ENV === 'development') {
+        //conf as per the env
+        config = devConfig;
+    } else if (process.env.NODE_ENV === 'production') {
+        config = prodConfig;
+    }
 }
 
 export default config;
