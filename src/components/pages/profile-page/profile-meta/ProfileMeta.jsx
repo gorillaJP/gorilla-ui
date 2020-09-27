01c31d7063e8605ad335c5aa8007cd394a1a36a4
dim912 <dim912@gmail.com>
2020-09-28 01:49:13 +0800

    edit icon on name of profile

--- src/components/pages/profile-page/profile-meta/ProfileMeta.jsx
+++ src/components/pages/profile-page/profile-meta/ProfileMeta.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import * as styles from "./ProfileMeta.module.css";
+import * as commonStyles from "../ProfilePage.module.css";
+import { PlusOutlined, FormOutlined } from "@ant-design/icons";
 
 const ProfileMeta = props => {
     return (
         <div className={styles.profileMeta}>
             <img src={props.imageUrl} className={styles.profileImage} />
-            <span className={styles.name}>{props.name}</span>
+            <div>
+                <span className={styles.name}>{props.name} &nbsp; </span>
+                <span className={commonStyles.editorIconSmall}>
+                    <FormOutlined />
+                </span>
+            </div>
             <span className={styles.email}>{props.email}</span>
         </div>
     );
