9fe78c4b7762ead341d4ae175af1c48a2435a9bc
shashithd <shashithd@gmail.com>
2020-10-11 02:06:40 +0800

    update footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,97 +1,99 @@
 import React, { useState } from "react";
 import { Row, Col } from "antd";
 import styles from "./Footer.module.css";
-import { DownOutlined, UpOutlined } from "@ant-design/icons";
+import Icon, {
+    DownOutlined,
+    UpOutlined,
+    FacebookFilled,
+    TwitterSquareFilled,
+    LinkedinFilled,
+    YoutubeFilled
+} from "@ant-design/icons";
 import { Container } from "../container/Container";
-const SubMenuArrow = props => {
-    return (
-        <span className={styles.menuArrowHead}>
-            {!props.keyVal ? (
-                <DownOutlined onClick={() => props.onClickFunc(props.keyName)} />
-            ) : (
-                <UpOutlined type="up" onClick={() => props.onClickFunc(props.keyName)} />
-            )}
-        </span>
-    );
-};
+import { Link } from "react-router-dom";
 
 const Footer = props => {
-    const [menuState, setMenuState] = useState({
-        jobSeekerMenu: false,
-        employerMenu: false,
-        stayConnectedMenu: false,
-        contactUsMenu: false
-    });
-
-    const setMenuStateFunc = menuName => {
-        let newState = { ...menuState };
-        newState[menuName] = !newState[menuName];
-        setMenuState(newState);
-    };
-
     return (
         <footer>
             <div className={styles.footer}>
                 <Container>
                     <Row>
-                        <Col xs={23} sm={23} md={5} lg={5} push={1}>
-                            <div className={styles.menuHeader}>
-                                <span>Job Seekers</span>
-                                <SubMenuArrow
-                                    keyName="jobSeekerMenu"
-                                    keyVal={menuState.jobSeekerMenu}
-                                    onClickFunc={setMenuStateFunc}
-                                />
+                        <Col
+                            xs={{ order: 4, span: 24 }}
+                            sm={{ order: 4, span: 24 }}
+                            md={{ order: 1, span: 9 }}
+                            lg={{ order: 1, span: 9 }}
+                        >
+                            <Link className={styles.companyName} to="/">
+                                GorillaJobs
+                            </Link>
+                            <span className={styles.address}>Address</span>
+                            <span className={styles.copyRight}>
+                                © 2020 Gorilla (Private) Limited. All rights reserved.
+                            </span>
+                            <div className={styles.socialMedia}>
+                                <span className={styles.header}>Stay Connected</span>
+                                <span className={styles.icons}>
+                                    <FacebookFilled
+                                        style={{ fontSize: "25px", color: "#1877f2", marginRight: "5px" }}
+                                    />
+                                    <TwitterSquareFilled
+                                        style={{ fontSize: "25px", color: "#1da1f2", marginRight: "5px" }}
+                                    />
+                                    <LinkedinFilled
+                                        style={{ fontSize: "25px", color: "rgb(0, 119, 181)", marginRight: "5px" }}
+                                    />
+                                    <YoutubeFilled style={{ fontSize: "25px", color: "#c00", marginRight: "5px" }} />
+                                </span>
                             </div>
-                            <ul className={menuState.jobSeekerMenu ? styles.showMenu : styles.menuItems}>
-                                <li>Job By Skill</li>
-                                <li>Job By Function</li>
-                                <li>Job By Company</li>
+                        </Col>
+                        <Col
+                            xs={{ order: 1, span: 24 }}
+                            sm={{ order: 1, span: 24 }}
+                            md={{ order: 2, span: 5 }}
+                            lg={{ order: 2, span: 5 }}
+                        >
+                            <div>
+                                <span className={styles.menuHeader}>Job Seekers</span>
+                            </div>
+                            <ul className={styles.menu}>
+                                <li>Search Jobs</li>
+                                <li>Job seekers login</li>
+                                <li>Upload/Build Resume</li>
+                                <li>Free Job Alerts</li>
+                                <li>Search Tips</li>
+                                <li>Help</li>
                             </ul>
                         </Col>
-                        <Col xs={23} sm={23} md={6} lg={6} push={1}>
-                            <div className={styles.menuHeader}>
-                                <span>Employers</span>
-                                <SubMenuArrow
-                                    keyName="employerMenu"
-                                    keyVal={menuState.employerMenu}
-                                    onClickFunc={setMenuStateFunc}
-                                />
+                        <Col
+                            xs={{ order: 2, span: 24 }}
+                            sm={{ order: 2, span: 24 }}
+                            md={{ order: 3, span: 5 }}
+                            lg={{ order: 3, span: 5 }}
+                        >
+                            <div>
+                                <span className={styles.menuHeader}>EMPLOYERS</span>
                             </div>
-                            <ul className={menuState.employerMenu ? styles.showMenu : styles.menuItems}>
-                                <li>Job By Skill</li>
-                                <li>Job By Function</li>
-                                <li>Job By Company</li>
+                            <ul className={styles.menu}>
+                                <li>Post Job</li>
+                                <li>Employer Login</li>
+                                <li>Price Packages</li>
                             </ul>
                         </Col>
-                        <Col xs={23} sm={23} md={6} lg={6} push={1}>
-                            <div className={styles.menuHeader}>
-                                <span>Stay Connected</span>
-                                <SubMenuArrow
-                                    keyName="stayConnectedMenu"
-                                    keyVal={menuState.stayConnectedMenu}
-                                    onClickFunc={setMenuStateFunc}
-                                />
+                        <Col
+                            xs={{ order: 3, span: 24 }}
+                            sm={{ order: 3, span: 24 }}
+                            md={{ order: 4, span: 5 }}
+                            lg={{ order: 4, span: 5 }}
+                        >
+                            <div>
+                                <span className={styles.menuHeader}>GORILLA</span>
                             </div>
-                            <ul className={menuState.stayConnectedMenu ? styles.showMenu : styles.menuItems}>
-                                <li>Gmail</li>
-                                <li>Facebook</li>
-                                <li>Twitter</li>
-                            </ul>
-                        </Col>
-                        <Col xs={23} sm={23} md={5} lg={5} push={1}>
-                            <div className={styles.menuHeader}>
-                                <span>Terms and Conditions</span>
-                                <SubMenuArrow
-                                    keyName="contactUsMenu"
-                                    keyVal={menuState.contactUsMenu}
-                                    onClickFunc={setMenuStateFunc}
-                                />
-                            </div>
-                            <ul className={menuState.contactUsMenu ? styles.showMenu : styles.menuItems}>
-                                <li>Terms</li>
-                                <li>Conditions</li>
-                                <li>FAQ</li>
+                            <ul className={styles.menu}>
+                                <li>About Us</li>
+                                <li>Testimonials</li>
+                                <li>Feedback</li>
+                                <li>Terms and Privacy</li>
                                 <li>Contact Us</li>
                             </ul>
                         </Col>
